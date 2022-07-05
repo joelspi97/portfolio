db2ba8aad31cc4f544f9377121c2ffb998d5b13e
Joel Spinelli <joelspi97@gmail.com>
2022-07-05 16:08:52 -0300

    fixed some bugs on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,36 +30,38 @@ function Header() {
         enableAnimations = !enableAnimations;
     }
 
-  return (
-    <header className="header section">
-        <Particles 
-            className="animated-background" 
-            id="tsparticles"
-            init={particlesInit}
-            options={particlesConfig} 
-        />
-        <nav className="header__nav">
-            <label htmlFor="toggle">
-                <div className="menu-btn" tabIndex="0" aria-label="Open menu">
-                    <div className="menu-btn__burger"></div>
+    return (
+        <header className="header section">
+            <Particles
+                className="animated-background"
+                id="tsparticles"
+                init={particlesInit}
+                options={particlesConfig}
+            />
+            <AnimatedDiv elementClassName="header__wrapper">
+                <label htmlFor="toggle">
+                    <nav className="menu-btn" tabIndex="0" aria-label="Open menu">
+                        <div className="menu-btn__burger"></div>
+                    </nav>
+                </label>
+                <input type="checkbox" id="toggle" />
+                <div className="menu">
+                    <button className="nav-link" type="button" onClick={pauseAnimations}>Pause animations</button>
+                    <nav>
+                        <a className="nav-link" href="#about-me">About me</a>
+                        <a className="nav-link" href="#projects">Projects</a>
+                        <a className="nav-link" href="#contact">Contact</a>
+                    </nav>
                 </div>
-            </label>
-            <input type="checkbox" id="toggle" />
-            <AnimatedDiv elementClassName="menu">
-                <button className="nav-link" type="button" onClick={pauseAnimations}>Pause animations</button>
-                <a className="nav-link" href="#about-me">About me</a>
-                <a className="nav-link" href="#projects">Projects</a>
-                <a className="nav-link" href="#contact">Contact</a>
             </AnimatedDiv>
-        </nav>
-        <AnimatedDiv elementClassName="header__content-container center-content">
-            <h1 className="section-heading">Front-end development &amp; Web Accessibility</h1>
-        </AnimatedDiv>
-        <AnimatedDiv>
-            <img src={profilePicture} alt="" />
-        </AnimatedDiv>
-    </header>
-  );
+            <AnimatedDiv elementClassName="header__content-container center-content">
+                <h1 className="section-heading">Front-end development &amp; Web Accessibility</h1>
+            </AnimatedDiv>
+            <AnimatedDiv>
+                <img src={profilePicture} alt="" />
+            </AnimatedDiv>
+        </header>
+    );
 }
 
 export default Header;
